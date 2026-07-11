a9c506c2901ce804642a821cd50fe18c8a595f77
MightyLabs-Web <auslabib@gmail.com>
2026-07-11 16:46:35 +1000

    minor fix

--- data.js
+++ data.js
@@ -3,5 +3,10 @@
 let members = [
     { id: 1, name: "Player1", role: "Tank", team: "", weapon1: "Thundercry Blade", weapon2: "Nameless Spear" },
     { id: 2, name: "Player2", role: "Healer", team: "", weapon1: "Panacea Fan", weapon2: "Soulshade Umbrella" },
-    { id: 3, name: "Player3", role: "DPS", team: "", weapon1: "Nameless Sword", weapon2: "Nameless Spear" }
+    { id: 3, name: "Player3", role: "DPS", team: "", weapon1: "Nameless Sword", weapon2: "Nameless Spear" },
+    { id: 4, name: "Enemy1", role: "ENEMEY", team: "", weapon1: "Stormbreaker Spear", weapon2: "Heavenquaker Spear" },
+    { id: 5, name: "Enemy2", role: "ENEMEY", team: "", weapon1: "Infernal Twinblades", weapon2: "Strategic Sword" },
+    { id: 6, name: "Enemy3", role: "ENEMEY", team: "", weapon1: "Mortal Rope Dart", weapon2: "Vernal Umbrella" },
+    { id: 7, name: "Enemy4", role: "ENEMEY", team: "", weapon1: "Inkwell Fan", weapon2: "Soulshade Umbrella" },
+    { id: 8, name: "Enemy5", role: "ENEMEY", team: "", weapon1: "Thundercry Blade", weapon2: "Nameless Spear" }
 ];
